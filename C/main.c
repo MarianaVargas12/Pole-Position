2d1d2b46628b87df03ede94f7ae16adc27917d62
Wajib Zaglul <wajib10@gmail.com>
2020-06-20 23:59:08 -0600

    Eliminacion archivos innecesarios

--- C/main.c
+++
@@ -1,9 +0,0 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h>
-#include <stdio.h>
-#include "sockets/Server.h"
-int main(int argc, char const *argv[])
-{
-    escuchaEnvia(8888,"Hola Marianaa");
-    //printf("%s\n", recibido );
-} 
